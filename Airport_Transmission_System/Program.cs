0d4e0dfb99f13502fccd81293698f0fb911fc58d
shivangChordia <shivangchordia02@gmail.com>
2024-11-16 18:57:52 -0500

    (Added Telemetry Files and a Basic Reader.)

--- Airport_Transmission_System/Program.cs
+++ Airport_Transmission_System/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace AircraftTransmissionSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aircraft Transmission System Started...");
+
+            // File paths for the telemetry data files
+            string[] filePaths = { "TelemetryFiles/C-QWWT.txt", "TelemetryFiles/C-GEFC.txt", "TelemetryFiles/C-FGAX.txt" };
+
+            // Start processing files for each aircraft
+            foreach (var filePath in filePaths)
+            {
+                if (!File.Exists(filePath))
+                {
+                    Console.WriteLine($"Error: File not found at {filePath}");
+                    continue;
+                }
+            }
+
+            Console.WriteLine("Transmission system is running...");
+        }
+
+    }
+}
