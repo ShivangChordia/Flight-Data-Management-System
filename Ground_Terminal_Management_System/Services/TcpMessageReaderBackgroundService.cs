cb79e29efbb93039b6f76efbade0efa6c955918e
shivangChordia <shivangchordia02@gmail.com>
2024-11-19 04:40:06 -0500

    added database connection and some work on UI for search and real time.

---
+++ Ground_Terminal_Management_System/Services/TcpMessageReaderBackgroundService.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace Ground_Terminal_Management_System.Services
+{
+    public class TcpMessageReaderBackgroundService : BackgroundService
+    {
+        private readonly TcpMessageReaderService _tcpMessageReaderService;
+
+        public TcpMessageReaderBackgroundService(TcpMessageReaderService tcpMessageReaderService)
+        {
+            _tcpMessageReaderService = tcpMessageReaderService;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await _tcpMessageReaderService.StartAsync(stoppingToken);
+        }
+    }
+}
